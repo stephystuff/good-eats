9279d6085836d842c7f3b05000c5f6bc8a1b7471
StephyStuff <stephanie.birch13@gmail.com>
2016-12-19 14:34:35 -0500

    Added states for home and sign-up view.

---
+++ src/js/good-eats.module.js
@@ -0,0 +1,26 @@
+(function() {
+  'use strict';
+
+  angular.module('good-eats', ['ui.router'])
+    .config(viewConfig);
+
+    viewConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+  function viewConfig($stateProvider, $urlRouterProvider){
+    $urlRouterProvider.when('', '/');
+
+    $stateProvider
+      .state({
+        name: 'home',
+        url: '/',
+        templateUrl: 'views/home.template.html'
+      })
+      .state({
+        name: 'sign-up',
+        url: 'sign-up',
+        templateUrl: 'views/sign-up.template.html'
+      });
+
+  }
+
+}());
