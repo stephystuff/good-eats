4f646e469c480f091e863f6028f764acf767bbf1
StephyStuff <stephanie.birch13@gmail.com>
2016-12-19 10:29:46 -0500

    Set up task and targets needed to for project. Will add options for testing when needed.

---
+++ gruntfile.js
@@ -0,0 +1,95 @@
+'use strict';
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+
+    clean: ['build/'],
+
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        ignores: ['node_modules/**']
+      },
+      source: {
+        files: {
+          src: ['src/js/**/*.js']
+        }
+      }
+    },
+    copy: {
+      html: {
+        files: [
+          {
+            expand: true,
+            cwd: 'src/',
+            src: 'index.html',
+            dest: 'build/'
+          }
+        ]
+      },
+        images: {
+          files: [
+            {
+              expand: true,
+              cwd: 'src/',
+              src: 'images/**',
+              dest: 'build/'
+            }
+          ]
+        },
+        concat: {
+          js: {
+            src: ['src/js/**/*.js'],
+            dest: 'build/js/app.js'
+          }
+        },
+        vendorjs: {
+          files: [
+            {
+              expand: true,
+              cwd: 'node_modules/angular/',
+              src: ['angular.js'],
+              dest: 'build/js'
+            }
+          ]
+        }
+      },
+      sass: {
+        allStyles: {
+          files: {
+            'build/css/styles.css': 'src/sass/main.scss'
+          }
+        }
+      },
+
+      watch: {
+        html: {
+          files: ['src/index.html'],
+          tasks: ['copy:html']
+        },
+        js: {
+          files: ['src/js/**/*.js'],
+          tasks: ['concat']
+        },
+        sass: {
+          files: ['src/sass/**/*.scss'],
+          tasks: ['sass']
+        },
+        images: {
+          files: ['src/images/**'],
+          tasks: ['copy:images']
+        }
+      }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('test', ['jshint']);
+    grunt.registerTask('default', [ 'clean', 'sass', 'copy', 'concat' ]);
+
+};
