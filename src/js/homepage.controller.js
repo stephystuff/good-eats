d18b03d85a7d70c3a3a08160a60de835f03ce270
StephyStuff <stephanie.birch13@gmail.com>
2016-12-20 14:22:05 -0500

    Created controller and function to change state from home to sign-up.

---
+++ src/js/homepage.controller.js
@@ -0,0 +1,19 @@
+(function() {
+  'use strict';
+
+  angular.module('good-eats')
+    .controller('HomePageController', HomePageController);
+
+  HomePageController.$inject = ['$state'];
+
+  function HomePageController($state){
+    this.signUp = function signUp(){
+      $state.go('sign-up');
+    };
+
+    this.tester = function tester() {
+      console.log('Working');
+    };
+  }
+
+}());
