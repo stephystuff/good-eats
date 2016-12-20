3e9547ea87d23e825e0f550c75aae7420943d469
StephyStuff <stephanie.birch13@gmail.com>
2016-12-20 14:17:01 -0500

    Included all html files in html copy task.

--- gruntfile.js
+++ gruntfile.js
@@ -81,7 +81,7 @@ module.exports = function(grunt) {
 
       watch: {
         html: {
-          files: ['src/index.html'],
+          files: ['src/**/*.html'],
           tasks: ['copy:html']
         },
         js: {
