a26fd714bd0f7e97c197f76df0df8e26e4cd5c89
StephyStuff <stephanie.birch13@gmail.com>
2016-12-21 11:01:22 -0500

    Created function to go to all-posts state.

---
+++ src/js/user-status.controller.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+
+  angular.module('good-eats')
+    .controller('UserStatusController', UserStatusController);
+
+  UserStatusController.$inject = ['$state'];
+
+  function UserStatusController($state) {
+    this.allPosts = function allPosts() {
+      $state.go('all-posts');
+    };
+  }
+
+}());
