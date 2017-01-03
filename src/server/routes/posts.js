c96944669cb353ea10dd62e3f956eb2c5e375811
StephyStuff <stephanie.birch13@gmail.com>
2017-01-03 09:44:34 -0500

    Fixed method not allowed error by creating a post router. Data now shows up in browser.

--- src/server/routes/posts.js
+++ src/server/routes/posts.js
@@ -7,26 +7,27 @@
   var router = express.Router();
 
   router.get('/', function allPosts(req, res) {
-    postsModel.getPosts(function dataRetrieved(err, data) {
-      if (err) {
-        console.error(err);
-        res.status(500).send('Uh oh...couldn\t get your data\n' + err );
-        return;
-      }
+    postsModel.getPosts(function retrieveData(err, data) {
       res.json(data);
     });
   });
 
   router.post('/', function createPost(req, res) {
-    console.log(req.body);
+    console.log('log 1',req.body);
 
-    postsModel.createPost(req.body, function dataCreated(err, data) {
+    postsModel.createPost(req.body, function createdData(err, data) {
       if (err) {
         console.error(err);
-        (500).send('Oh no...couldn\t post your data');
+        res.status(500).send('couldn\t get your data');
         return;
       }
-      res.json(data);
+      res.json({
+        'dishName': data.ops[0].dishName,
+        'restaurant': data.ops[0].restaurant,
+        'cuisineType': data.ops[0].cuisineType,
+        'photo': data.ops[0].photo,
+        'comment': data.ops[0].comment
+      });
     });
   });
 
