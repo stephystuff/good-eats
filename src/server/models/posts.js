2289e30ff4bdacb58e075ecaaaa6d2cb70d61589
StephyStuff <stephanie.birch13@gmail.com>
2017-01-09 19:48:45 -0500

    Added variable for ObjectId. Modified functions for getPost and deletePost. Now able to retrieve single post, but still unable to delete a post.

--- src/server/models/posts.js
+++ src/server/models/posts.js
@@ -1,83 +1,88 @@
-var dbConnect = require('./db-connect');
+(function() {
+  'use strict';
 
-module.exports = {
-  getPosts,
-  getPost,
-  createPost,
-  deletePost,
-  // updatePost
-};
+  var dbConnect = require('./db-connect');
+  var ObjectId = require('mongodb').ObjectId;
 
-function getPosts(done) {
-  dbConnect(function connectHandler(err,db) {
-    if (err) {
-      done(err, null);
-      return;
-    }
-    db.collection('posts')
-      .find()
-      .toArray(done);
-  });
-}
+  module.exports = {
+    getPosts,
+    getPost,
+    createPost,
+    deletePost
+    // updatePost
+  };
 
-function createPost(data, done) {
-  dbConnect(function connectHandler(err, db) {
-    if (err) {
-      done(err, null);
-      return;
-    }
-    data.createTime = Date.now();
-    db.collection('posts')
-      .insert(data, done);
-  });
-}
+  function getPosts(done) {
+    dbConnect(function connectHandler(err,db) {
+      if (err) {
+        done(err, null);
+        return;
+      }
+      db.collection('posts')
+        .find()
+        .toArray(done);
+    });
+  }
 
-function getPost(data, done){
-  dbConnect(function connectHandler(err, db) {
-    if (err) {
-      done(err, null);
-      return;
-    }
-    var post = db.collection('posts')
-      .findOne({_id: ObjectId(data)}, function(err, data) {
-        newData = {
-          'id': data._id,
-          'dishName': data.dishName,
-          'restaurant': data.restaurant,
-          'cuisineType': data.cuisineType,
-          'photo': data.photo,
-          'comment': data.comment,
-          'createTime': data.createTime
-        };
-        done(null, data);
-      });
-  });
-}
+  function createPost(data, done) {
+    dbConnect(function connectHandler(err, db) {
+      if (err) {
+        done(err, null);
+        return;
+      }
+      data.createTime = Date.now();
+      db.collection('posts')
+        .insert(data, done);
+    });
+  }
 
-function deletePost(data, done) {
-  console.log(data);
-  dbConnect(function connectHandler(err, db) {
-    if (err) {
-      done(err, null);
-      return;
-    }
-    db.collection('posts')
-        .findOne({_id: ObjectId(data)}, function(err, data) {
+  function getPost(id, done){
+    dbConnect(function connectHandler(err, db) {
+      if (err) {
+        done(err, null);
+        return;
+      }
+      db.collection('posts')
+        .findOne({_id: new ObjectId(id)}, function(err, data) {
           var newData = {
             'id': data._id,
             'dishName': data.dishName,
             'restaurant': data.restaurant,
             'cuisineType': data.cuisineType,
             'photo': data.photo,
-            'comment': data.comment
+            'comment': data.comment,
+            'createTime': data.createTime
           };
+          done(null, newData);
         });
-    db.collection('posts')
-        .findOneAndDelete({_id: ObjectId(data)})
-          .then(function successHandler(finish){
-            done(err, newData);
-          }, function errorHandler(value){
-            done(value, null);
+    });
+  }
+
+  function deletePost(id, done) {
+    dbConnect(function connectHandler(err, db) {
+      if (err) {
+        done(err, null);
+        return;
+      }
+      db.collection('posts')
+          .findOne({_id: ObjectId(id)}, function(err, data) {
+          var newData = {
+              'id': data._id,
+              'dishName': data.dishName,
+              'restaurant': data.restaurant,
+              'cuisineType': data.cuisineType,
+              'photo': data.photo,
+              'comment': data.comment,
+              'createTime': data.createTime
+            };
           });
-  });
-}
+      db.collection('posts')
+          .findOneAndDelete({_id: ObjectId(id)})
+          .then(function successHandler(finish){
+              done(err, newData);
+            }, function errorHandler(value){
+              done(value, null);
+            });
+    });
+  }
+}());
