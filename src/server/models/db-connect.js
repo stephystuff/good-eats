a9ed1717c31b8b16af25a175145c637194821d46
StephyStuff <stephanie.birch13@gmail.com>
2017-01-11 12:36:58 -0500

    Changed MONGODB_URL to MONGODB_URI.

--- src/server/models/db-connect.js
+++ src/server/models/db-connect.js
@@ -2,7 +2,7 @@
   'use strict';
 
   var mongodb = require('mongodb');
-  var uri = process.env.MONGODB_URL || 'mongodb://localhost:27017/good-eats';
+  var uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/good-eats';
 
   module.exports = function connect(done) {
     mongodb.MongoClient.connect(uri, done);
